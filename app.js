5077ee77dfb7d8647ebff0b86fba32c72897a33f
yash2626 <yashprajapati672@gmail.com>
2020-10-05 03:57:37 -0700

    make it better

--- app.js
+++ app.js
@@ -5,15 +5,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const myUsersRoute = require("./routes//myUsersRoute");
 const userRoute = require("./routes/userRoute");
-//const Pusher = require("pusher");
 const app = express();
-// const pusher = new Pusher({
-//   app_id: "1072697",
-//   key: "951df5bf123c0460974d",
-//   secret: "8f915e2fe6c8cc4dbae2",
-//   cluster: "ap2",
-//   encrypted: true,
-// });
+
 app.use(bodyParser.json());
 app.use(fileUpload());
 app.use("/uploads", express.static(path.join("uploads")));
@@ -26,20 +19,6 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Methods", "PATCH,POST,GET,DELETE");
   next();
 });
-
-// const db = mongoose.connection;
-
-// db.once("open", () => {
-//   const msgCollection = db.collection("myusers");
-//   const changeStream = msgCollection.watch();
-
-//   changeStream.on("change", (change) => {
-//     if (change.operationType === "insert") {
-//       const messageDetails = change.fullDocument;
-//       pusher.trigger("messages", "inserted", messageDetails);
-//     }
-//   });
-// });
 
 app.use("/api/myusers", myUsersRoute);
 app.use("/api/user", userRoute);
